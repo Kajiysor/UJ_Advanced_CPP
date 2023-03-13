b1f29f7322c6b71d96021fe8ee34af83ac710ec7
Jonasz Gawlik <mrbarman55@gmail.com>
2023-03-13 16:18:36 +0100

    clang-format

--- Ex1/include/templates.hpp
+++ Ex1/include/templates.hpp
@@ -4,81 +4,83 @@
 
 namespace Ex1
 {
-    template <typename T>
-    T max(T a, T b) { return a > b ? a : b; }
+template <typename T>
+T max(T a, T b)
+{
+    return a > b ? a : b;
+}
 
-    template <typename T, typename U>
-    T convert(U u)
+template <typename T, typename U>
+T convert(U u)
+{
+    try
     {
-        try
-        {
-            return static_cast<T>(u);
-        }
-        catch (const std::exception &e)
-        {
-            std::cerr << "Exception: " << e.what() << std::endl;
-            return T();
-        }
+        return static_cast<T>(u);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        return T();
+    }
+}
+
+template <>
+std::string convert<std::string>(double d)
+{
+    return std::to_string(d);
+}
+
+template <std::size_t N, typename T>
+T dot(const std::array<T, N> &a, const std::array<T, N> &b)
+{
+    T result = T();
+    for (std::size_t i = 0; i < N; ++i)
+    {
+        result += a[i] * b[i];
+    }
+    return result;
+}
+
+template <template <int N> class T, int K>
+void foo(T<K> t)
+{
+    std::cout << K << std::endl;
+}
+
+template <int N>
+class Bar
+{};
+
+template <typename T>
+class Stack
+{
+public:
+    typedef T value_type;
+
+    void push(const T &t) { s.push_back(t); }
+
+    T pop()
+    {
+        T t = s.back();
+        s.pop_back();
+        return t;
     }
 
-    template <>
-    std::string convert<std::string>(double d)
+    bool empty() const { return s.empty(); }
+
+private:
+    std::vector<T> s;
+};
+
+template <typename T>
+decltype(auto) sum(T t)
+{
+    typename T::value_type sum = 0;
+    while (!t.empty())
     {
-        return std::to_string(d);
+        sum += t.pop();
     }
+    return sum;
+}
 
-    template <std::size_t N, typename T>
-    T dot(const std::array<T, N> &a, const std::array<T, N> &b)
-    {
-        T result = T();
-        for (std::size_t i = 0; i < N; ++i)
-        {
-            result += a[i] * b[i];
-        }
-        return result;
-    }
-
-    template <template <int N> class T, int K>
-    void foo(T<K> t)
-    {
-        std::cout << K << std::endl;
-    }
-
-    template <int N>
-    class Bar
-    {
-    };
-
-    template <typename T>
-    class Stack
-    {
-    public:
-        typedef T value_type;
-
-        void push(const T &t) { s.push_back(t); }
-
-        T pop()
-        {
-            T t = s.back();
-            s.pop_back();
-            return t;
-        }
-
-        bool empty() const { return s.empty(); }
-
-    private:
-        std::vector<T> s;
-    };
-
-    template <typename T>
-    decltype(auto) sum(T t)
-    {
-        typename T::value_type sum = 0;
-        while (!t.empty())
-        {
-            sum += t.pop();
-        }
-        return sum;
-    }
-
-} // namespace Ex1
+}  // namespace Ex1
